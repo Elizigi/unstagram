1c5cc407087079a939f5daa7c994605314c77e1b
Elizigi <elizigi876@gmail.com>
2025-11-26 17:38:15 +0200

    init

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello TypeScript Express!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
