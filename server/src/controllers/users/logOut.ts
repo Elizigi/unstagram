7a9a233e647b4e18c496a7c2492bffead5cf2fb7
Elizigi <elizigi876@gmail.com>
2025-11-30 19:36:54 +0200

    logout path

---
+++ server/src/controllers/users/logOut.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+async function logOut(req: Request, res: Response) {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged out" });
+  } catch (error) {
+    console.error("Error Logging Out", error);
+    res
+      .status(500)
+      .json({ success: false, message: "error logging out", error });
+  }
+}
+
+export default logOut;
--- server/src/routes/user/userRoutes.ts
+++ server/src/routes/user/userRoutes.ts
@@ -2,9 +2,13 @@ import express from "express";
 import userRegister from "../../controllers/users/userRegister";
 import userLogin from "../../controllers/users/userLogin";
 import userLogged from "../../controllers/users/userLogged";
+import logOut from "../../controllers/users/logOut";
+import userAuth from "../../controllers/users/userAuth";
 const router = express.Router();
 
 router.post("/user-register", userRegister);
 router.post("/user-login", userLogin);
 router.get("/user-logged", userLogged);
+router.get("/user-logout",userAuth, logOut);
+
 export default router;
