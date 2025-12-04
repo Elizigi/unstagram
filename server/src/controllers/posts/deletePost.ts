269d5da43c71fc33850b3e9b85ec5b06ad47d0f0
Elizigi <elizigi876@gmail.com>
2025-12-04 21:12:25 +0200

    updated

--- server/src/controllers/posts/deletePost.ts
+++ server/src/controllers/posts/deletePost.ts
@@ -1,18 +1,33 @@
 import { Request, Response } from "express";
 import { pool } from "../../server";
-import { ResultSetHeader } from "mysql2";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 async function deletePost(req: Request, res: Response) {
   try {
     const { post_id } = req.body;
-
-    const [result] = await pool.execute<ResultSetHeader>(
-      "DELETE FROM posts WHERE post_id=?",
+    const userId = req.userId;
+    if (!userId) {
+      throw new Error(`no user id: ${userId}`);
+    }
+    const [rows] = await pool.execute<RowDataPacket[]>(
+      "SELECT user_id FROM posts WHERE post_id = ?",
       [post_id]
     );
-    if (result.affectedRows === 0) {
-      throw new Error(`post does not exists post id: ${post_id}`);
+    if (rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "post does not exist",
+      });
     }
+    if (rows[0].user_id !== userId) {
+      return res.status(403).json({
+        success: false,
+        message: "no access to delete this post",
+      });
+    }
+    await pool.execute<ResultSetHeader>("DELETE FROM posts WHERE post_id = ?", [
+      post_id,
+    ]);
     res.status(200).json({ success: true, message: "post deleted", post_id });
   } catch (error) {
     console.error("Error Deleting Post", error);
