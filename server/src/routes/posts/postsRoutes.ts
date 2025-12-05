980eac8c3f4462f214c6528c0d14040d30b6b357
Elizigi <elizigi876@gmail.com>
2025-12-05 23:02:44 +0200

    post follow api

--- server/src/routes/posts/postsRoutes.ts
+++ server/src/routes/posts/postsRoutes.ts
@@ -5,11 +5,13 @@ import getAllPosts from "../../controllers/posts/postGetAll";
 import { optionalAuth } from "../../controllers/users/optionalAuth";
 import changeLikeStatus from "../../controllers/posts/postLike";
 import deletePost from "../../controllers/posts/deletePost";
+import getFollowedPosts from "../../controllers/posts/getFollowedPosts";
 const router = express.Router();
 
 router.get("/post-getall", optionalAuth, getAllPosts);
 router.post("/post-create", userAuth, createPost);
 router.post("/post-like", userAuth, changeLikeStatus);
 router.post("/post-delete", userAuth, deletePost);
+router.get("/post-followed", optionalAuth, getFollowedPosts);
 
 export default router;
