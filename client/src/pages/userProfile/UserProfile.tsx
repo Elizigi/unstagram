d5bf2cde8e9c9f5469c24e8be5e6d72243b51264
Elizigi <elizigi876@gmail.com>
2025-12-07 23:09:53 +0200

    api call

--- client/src/pages/userProfile/UserProfile.tsx
+++ client/src/pages/userProfile/UserProfile.tsx
@@ -1,9 +1,15 @@
 import styles from "./UserProfile.module.scss";
+import UserProfileMV from "./UserProfileMV";
 
-const UserProfile = () => {
-  return <div className={styles.profileContainer}>
-    
-  </div>;
+const UserProfile = (userId: number) => {
+  const { profileName } = UserProfileMV(userId);
+  return (
+    <div className={styles.profileContainer}>
+      <div className={styles.userCard}>
+        <div className={styles.fakeImg}></div> <h2>{profileName}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
---
+++ client/src/pages/userProfile/UserProfileMV.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const UserProfileMV = (userId: number) => {
+  const [profileName, setProfileName] = useState();
+
+  useEffect(() => {
+    const fetchProfile = async (userId: number) => {
+      const response = await fetch(
+        `http://localhost:3000/api/users/fetch-profile/${userId}`,
+        { credentials: "include" }
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data.name) {
+        setProfileName(data.name);
+      }
+    };
+    fetchProfile(userId);
+  }, [userId]);
+  return { profileName };
+};
+
+export default UserProfileMV;
