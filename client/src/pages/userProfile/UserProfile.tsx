993bad159e8767791c7addb47e8cb15038aa03cf
Elizigi <elizigi876@gmail.com>
2025-12-07 10:20:55 +0200

    small change

--- client/src/pages/userProfile/UserProfile.tsx
+++ client/src/pages/userProfile/UserProfile.tsx
@@ -1,7 +1,9 @@
 import styles from "./UserProfile.module.scss";
 
 const UserProfile = () => {
-  return <div className={styles.profileContainer}></div>;
+  return <div className={styles.profileContainer}>
+    
+  </div>;
 };
 
 export default UserProfile;
