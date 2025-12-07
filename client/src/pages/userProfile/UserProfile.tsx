897165afb98e7ff637fb8b02fc6aa682a2add831
Elizigi <elizigi876@gmail.com>
2025-12-07 23:59:59 +0200

    profile  simple

--- client/src/pages/userProfile/UserProfile.tsx
+++ client/src/pages/userProfile/UserProfile.tsx
@@ -6,11 +6,39 @@ interface UserProfileProps {
   userId: number;
 }
 const UserProfile: FC<UserProfileProps> = ({ userId }) => {
-  const { profileName } = UserProfileMV(userId);
+  const { profileName,posts } = UserProfileMV(userId);
   return (
     <div className={styles.profileContainer}>
       <div className={styles.userCard}>
         <div className={styles.fakeImg}></div> <h2>{profileName}</h2>
+        {[...posts].reverse().map((post) => (
+          <div className={styles.post} key={post.post_id + post.post_title}>
+            <div
+              className={styles.postCreator}
+              role="toolbar"
+              onKeyDown={() => {}}
+            >
+              <div className={styles.fakeImg}></div>
+
+              <h3>{post.user_name}</h3>
+              {Number(userId) === Number(post.user_id) ? (
+                <button className={styles.deleteBtn}>Too Cringe</button>
+              ) : (
+                <button
+                  className={styles.followBtn}
+                >
+                 
+                </button>
+              )}
+            </div>
+            <h2 className={styles.postTitle}>{post.post_title}</h2>
+            {post.post_img_url && (
+              <img src={post.post_img_url} alt={post.post_title} />
+            )}
+            <p>{post.post_description}</p>
+           
+          </div>
+        ))}
       </div>
     </div>
   );
--- client/src/pages/userProfile/UserProfileMV.tsx
+++ client/src/pages/userProfile/UserProfileMV.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import type { Post } from "../../model/postModel";
 
 const UserProfileMV = (userId: number) => {
   const [profileName, setProfileName] = useState();
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     const fetchProfile = async (userId: number) => {
@@ -11,13 +13,14 @@ const UserProfileMV = (userId: number) => {
       );
       const data = await response.json();
       console.log(data);
-      if (data.name) {
-        setProfileName(data.name);
+      if (data.profile.user_name) {
+        setProfileName(data.profile.user_name);
+        setPosts(data.profile.posts);
       }
     };
     fetchProfile(userId);
   }, [userId]);
-  return { profileName };
+  return { profileName, posts };
 };
 
 export default UserProfileMV;
